0bf2e406e5d36feefbaf22bc902d831317849271
sergioalvarezpro <ealvser@gmail.com>
2022-10-17 15:33:23 +0200

    CPT - labels
    Definimos labels

--- app/public/wp-content/plugins/plugin-cpt/plugin-cpt.php
+++ app/public/wp-content/plugins/plugin-cpt/plugin-cpt.php
@@ -11,7 +11,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       cafelog
  * Domain Path:       /languages
  */
 
@@ -40,7 +40,42 @@ function prueba($content){
 
 // Damos de alta el CPT: Comic Reviews
 function comic_review_setup_post_type(){
- register_post_type( 'cafelog_comic_review' )
+
+    $labels = array(
+        'name' => __( 'Reseñas', 'cafelog' ),
+        'singular_name' => __('Reseña', 'cafelog'),
+        'add_new' => __('Añadir reseña', 'cafelog'),
+        'add_new_item' => __('Añadir nueva reseña', 'cafelog'),
+        'edit_item' => __('Editar reseña', 'cafelog'),
+        'new_item' => __('Nueva reseña', 'cafelog'),
+        'view_item' => __('Ver reseña', 'cafelog'),
+        'view_items' => __('Ver reseñas', 'cafelog'),
+        'search_items' => __('Buscar reseñas', 'cafelog'),
+        'not_found' => __('No se encontraron reseñas', 'cafelog'),
+        'not_found_in_trash' => __('Ninguna reseña encontrada en la papelera', 'cafelog'),
+        //'parent_item_colon'
+        'all_items' => __('Todas las reseñas', 'cafelog'),
+        //'archives'
+        //'attributes'
+        'insert_into_item' => __('Insertar en la reseña', 'cafelog'),
+        //'uploaded_to_this_item'
+        //'featured_image'
+        //'set_featured_image'
+        //'remove_featured_image'
+        //'use_featured_image'
+        //'menu_name' => __('Reseñas de cómics', 'cafelog')
+        'filter_items_list' => __('Lista de reseñas filtradas', 'cafelog'),
+        'items_list_navigation' => __('Navegación por el listado de reseñas', 'cafelog'),
+        'items_list' => __('Lista de reseñas', 'cafelog'),
+        //'name_admin_bar' => __('Cualquiera', 'cafelog')
+    );
+
+    $args = array(
+        'public' => true,
+        'labels' => $labels,
+    );
+
+ register_post_type( 'cafelog_comic_review', $args )
 }
 
 add_action( 'init', 'comic_review_setup_post_type' );
